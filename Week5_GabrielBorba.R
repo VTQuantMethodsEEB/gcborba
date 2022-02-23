3e30e4b7633dc9791713016b43ef58b48391b826
gcborba <you@example.com>
2022-02-23 17:03:12 -0500

    update code

--- Week5_GabrielBorba.R
+++ Week5_GabrielBorba.R
@@ -1,8 +1,10 @@
 rm(list=ls())
 #loading package used
 library(coin)
-
-
+library(ggplot2)
+library(tidyverse)
+library(tidyr)
+library(dplyr)
 #using the catch data - read
 catdat=read.csv("catch.csv") 
 hyd=read.csv("hydro.csv")
@@ -11,71 +13,98 @@ hyd=read.csv("hydro.csv")
 #Formulate two different hypotheses about your data, one that can be tested 
 #using a permutation test and one that can be tested using a “classical” test.
 
-#H1= the mean of catch success are significantly different for boat length
-#H2= the mean of catch success are significantly different for catch effort
+#H1 = the catch values is significantly higher in larger boat length than smaller boats. 
 
-#I get if my variables are normal by visualizing a histogram of frequency 
-hist(catdat$length)
-hist(catdat$success)
+#Split my boat length data into small and large values 
+small<-catdat %>%
+  group_by(catch) %>%
+  filter(length < mean(length, na.rm = TRUE)) %>%
+  summarise(catch,length )
 
-#Cause my data set is larger than 50000 so R does not allow me to run a Shapiro Test
+large<-catdat %>%
+  group_by(catch) %>%
+  filter(length > mean(length, na.rm = TRUE)) %>%
+  summarise(catch,length )
+
+#create a new data frame and aggregate boats groups values 
+fish <- data.frame(
+  length=rep(c("small","large"),
+             c(length(small$length), length(large$length))),
+  catch=c(small$catch, large$catch)
+)
+
+View(fish)
+
+#My data set is larger than 50000 so R does not allow me to run a Shapiro Test
 shapiro.test(catdat$length)#normal data distributed 
 shapiro.test(catdat$success)
 
+#So, I check if my variables are normal by visualizing a histogram of frequency 
+
+hist(small$catch)
+hist(small$length)
+
+
+
 #it turns that they are not normal, so I will run a non-parametric test
-t.h1 <- t.test(length~success,data=catdat,var.equal=TRUE)
 
-stripchart(length~factor(success),pch=19, at=c(1.3,1.7), v=T,data=catdat)
-kruskal_test(length~factor(success),data=catdat)
+wilcox.test(catch~factor(length),data=fish)
 
+# p<0.05, catch values from small and large boat length are nonidentical populations, they have different data distribution
 
+#Plot
+theme_set(theme_bw()+ #set my theme as a default for my plot 
+            theme(axis.title=element_text(size=23),
+                  axis.text=element_text(size=15),
+                  panel.grid = element_blank(), 
+                  axis.line=element_line(),
+                  axis.text.x = element_text("")))
+stripchart(catch~factor(length),data=fish,pch=19, at=c(1.3,1.7), v=T)
+points(mean(fish$catch), col="red")
+x11()
+g1=ggplot(data=fish,aes(x=factor(length),y=log(catch)))+
+    geom_boxplot()+
+  geom_point() 
+g1+xlab("Boat length")
 
 
 #### permutation tests######
-#Permutation test using my H2 = the mean of catch success are significantly different for catch effort
-oneway_test(effort~factor(success),data=catdat)
-
-oneway_test(effort~factor(success),data=catdat,distribution="exact")
+#Permutation test using my H1 = the mean of catch success is significantly higher in larger boat length than smaller boats. 
+oneway_test(catch~factor(length),data=fish)
 
 oneway_test(effort~factor(success),data=catdat,distribution=approximate(nresample=9999))
 
 
-#the mean of catch are significantly different for river 
-ama=subset(catdat$catch,catdat$river=="amz")
-mad=subset(catdat$catch,catdat$river=="mad")
-#4. Try to use a loop for your permutation test (e.g. a brute force method).  
+#Try to use a loop for your permutation test (e.g. a brute force method).  
 #Use any classical test you learned about to test your other hypothesis.
-set.seed(100)
+
+set.seed(101)
 #set.seed will set R's random number generator to start at the same place
 #this ensures that when you, and I, and anyone else, does the test, we will all get the same results
 
 result <- NA 
 
+#the mean of catch are significantly different for rivers 
+ama=subset(catdat$catch,catdat$river=="amz", na.omit=T)
+mad=subset(catdat$catch,catdat$river=="mad")
 
-for (i in 1:100) {
+for (i in 1:1000) {
   catchboot <- sample(c(ama,mad)) ## scramble
   ## 
-  amaboot <- catchboot[1:length(ama)]  #this says assign the first six colonies to forest
-  madboot <- catchboot[(length(mad)+1):length(catchboot)] #assign the rest of the colonies to field
+  amaboot <- catchboot[1:length(ama)]  #this says assign the first values 
+  madboot <- catchboot[(length(ama)+1):length(catchboot)] #assign the rest of the colonies to field
   
   ## compute & store difference in means
-result[i] <- mean(amaboot)-mean(madboot) #calculate the difference in the field means and the forest means
+result[i] <- mean(madboot)-mean(amaboot) #calculate the difference in river means
   #[i] says "where i", and i is a counter, after running this loop, i should be 1000
 }
 
-#what is our observed mean difference?
-obs <- mean(ama)-mean(mad)
+#what is my observed mean difference?
+obs <- mean(mad)-mean(ama)
 obs
-result[result>=obs]
-length(result[result>=obs])
-235/100
+
 mean(result>=obs)    
 
-#we could double the p-value
-2*mean(result>=obs)          ## doubling
-
-#or we could count the area in both tails
-mean(abs(result)>=abs(obs))
-
-#testing 
-t.test(result)
+#my observed mean difference is very far from my mean difference from permutation test 
+hist(result,col="gray",las=1,main="", xlim= c(-100, 100))
+abline(v=obs,col="red")
