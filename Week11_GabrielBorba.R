71fa4cc12ab8f61242af2e582aca722cdc0d8c94
gcborba <you@example.com>
2022-04-13 17:58:01 -0400

    update code

--- Week11_GabrielBorba.R
+++ Week11_GabrielBorba.R
@@ -13,6 +13,7 @@ library(AICcmodavg)
 catdat=read.csv("catch.csv") 
 #round my catch values - inflated model, consider binomial or gamma with Tweedie Distribution
 catdat$catch1 <- round(catdat$catch) #considering counting values - rounded catch
+
 #Use likelihood ratio tests and one other model selection approach to test at 
 #least 3 models of your data.
 c1 = glm.nb(catch1~1,data = catdat) #null model 
@@ -22,35 +23,26 @@ c3 = glm.nb(catch1~length+river,data = catdat)
 summary(c3)
 c4 = glm.nb(catch1~length*river,data = catdat)
 summary(c4)
-#this test length vs length + river
+#this test catch vs length + river
 anova(c1,c2)
-#this test light vs light *time
+#this test catch vs light *time
 anova(c1,c3)
 #can look at all 3
 anova(c1,c2,c3)
 anova(c4,c3)
 
 #Explain what the results are telling you for each approach.
+
+
+
+
 ##another example using caret##
 library(caret)
 library(psych)
 
-#from: https://quantdev.ssri.psu.edu/tutorials/cross-validation-tutorial
-
-data <- sat.act
-head(data)
-
-#models to compare
-
-mod_1 = lm(ACT ~ gender + age + SATV + SATQ,   data = data)
-summary(mod_1)
-
-mod_2 = lm(ACT ~  gender + age ,   data = data)
-summary(mod_2)
-
-data_ctrl <- trainControl(method = "cv", number = 5)
+data_ctrl <- trainControl(method = "cv", number = 10)
 #We first set up the number of folds for cross-validation by defining the training control. 
-#In this case, we chose 5 folds, but the choice is ulimately up to you.
+#In this case, I chose 10 folds.
 
 #run model with cross valdation
 mod_1_caret <- train(catch1 ~ length + river,   # model to fit
@@ -74,7 +66,7 @@ mod_2_caret$resample
 sd(mod_1_caret$resample$Rsquared)
 
 ##now model 2
-#run model with cross valdation
+#run model with cross validation
 mod_2_caret <- train(catch1 ~  length * river,   # model to fit
                      data = catdat,                        
                      trControl = data_ctrl,              # folds
@@ -83,12 +75,45 @@ mod_2_caret <- train(catch1 ~  length * river,   # model to fit
 
 mod_2_caret
 #We find that after using 5-fold cross-validation, 
-#our model also accounts for 1% of the variance (R-squared = 0.014) in ACT scores for these participants.
-
-#mod_1 is the better model length + river
+#mod_1 is the best model length + river
 #sd(mod_2_caret$resample$Rsquared)
 #0.0003856321
 #sd(mod_1_caret$resample$Rsquared)
 #0.0006616286
-#the accuracy of cross-validation and the parameters from the whole sample should be reported.
-#Include a synthesis statement on how the output of each approach is similar or different in your code. Remember to update your README and annotate your code.
+n1 = glm.nb(catch1~length,data = catdat)
+n2 = glm.nb(catch1~river,data = catdat)
+n3 = glm.nb(catch1~length+river,data = catdat)
+n4 = glm.nb(catch1~length*river,data = catdat)
+summary(n4)
+#for AICc
+n=nrow(catdat)#or whatever the length of your df is
+tabA = AIC(n1,n2,n3,n4)
+#it would be nice to have AICC for a dataset this small
+tabA$k<-c(n1$rank,n2$rank,n3$rank,n4$rank)
+tabA$aiccs<-tabA$AIC+((2*tabA$k*(tabA$k+1))/(n-tabA$k-1))
+#now order from smallest to biggest
+tabA=tabA[order(tabA$aiccs),]
+#calculate delta AIC
+tabA$dAIC = tabA$aiccs - min(tabA$aiccs)
+#you use the next two lines to get weights
+tabA$edel<-exp(-0.5*tabA$dAIC) 
+tabA$wt<-tabA$edel/sum(tabA$edel)
+tabA
+
+#what issue do we have here?
+n5 = glm.nb(catch1~1,data = catdat)
+
+#now run this all again with n5!
+tabA = AIC(n1,n2,n3,n4,n5)
+#it would be nice to have AICC for a dataset this small
+tabA$k<-c(n1$rank,n2$rank,n3$rank,n4$rank,n5$rank)
+tabA$aiccs<-tabA$AIC+((2*tabA$k*(tabA$k+1))/(n-tabA$k-1))
+#now order from smallest to biggest
+tabA=tabA[order(tabA$aiccs),]
+#calculate delta AIC
+tabA$dAIC = tabA$aiccs - min(tabA$aiccs)
+#you use the next two lines to get weights
+tabA$edel<-exp(-0.5*tabA$dAIC) 
+tabA$wt<-tabA$edel/sum(tabA$edel)
+tabA
+
