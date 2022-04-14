ff8a3a1263fa55a646f5faa5604dbe265484cf82
gcborba <you@example.com>
2022-04-13 21:10:51 -0400

    update week 11

--- Week11_GabrielBorba.R
+++ Week11_GabrielBorba.R
@@ -28,13 +28,44 @@ anova(c1,c2)
 #this test catch vs light *time
 anova(c1,c3)
 #can look at all 3
-anova(c1,c2,c3)
-anova(c4,c3)
+anova(c1,c2,c3,c4,test = "LRT")
 
 #Explain what the results are telling you for each approach.
+#The likelihood ratio tests for model comparison shows the simple model including 
+#only boat length is the one which has the lower difference in numbers of parameters 
+#Based on the likelihood ratio test, length model is my best model that explained the relationship between catch and boat length from river sites. 
 
 
 
+#AIC selection model
+n1 = glm.nb(catch1~length,data = catdat)
+n2 = glm.nb(catch1~river,data = catdat)
+n3 = glm.nb(catch1~length+river,data = catdat)
+n4 = glm.nb(catch1~length*river,data = catdat)
+summary(n4)
+n5 = glm.nb(catch1~1,data = catdat)
+#for AICc
+n=nrow(catdat)#or whatever the length of your df is
+
+tabA = AIC(n1,n2,n3,n4,n5)
+#it would be nice to have AICC for a dataset this small
+tabA$k<-c(n1$rank,n2$rank,n3$rank,n4$rank,n5$rank)
+tabA$aiccs<-tabA$AIC+((2*tabA$k*(tabA$k+1))/(n-tabA$k-1))
+#now order from smallest to biggest
+tabA=tabA[order(tabA$aiccs),]
+#calculate delta AIC
+tabA$dAIC = tabA$aiccs - min(tabA$aiccs)
+#you use the next two lines to get weights
+tabA$edel<-exp(-0.5*tabA$dAIC) 
+tabA$wt<-tabA$edel/sum(tabA$edel)
+tabA
+
+#After ran each model structure, I checked model has the best weight based on dAIC. 
+#The interactive model (n4) showed the high weight and lower dAIC from the rank 
+#selection against the other model structures. 
+
+
+######Extra - cross validation
 
 ##another example using caret##
 library(caret)
@@ -80,27 +111,3 @@ mod_2_caret
 #0.0003856321
 #sd(mod_1_caret$resample$Rsquared)
 #0.0006616286
-
-#AIC selection model
-n1 = glm.nb(catch1~length,data = catdat)
-n2 = glm.nb(catch1~river,data = catdat)
-n3 = glm.nb(catch1~length+river,data = catdat)
-n4 = glm.nb(catch1~length*river,data = catdat)
-summary(n4)
-n5 = glm.nb(catch1~1,data = catdat)
-#for AICc
-n=nrow(catdat)#or whatever the length of your df is
-
-tabA = AIC(n1,n2,n3,n4,n5)
-#it would be nice to have AICC for a dataset this small
-tabA$k<-c(n1$rank,n2$rank,n3$rank,n4$rank,n5$rank)
-tabA$aiccs<-tabA$AIC+((2*tabA$k*(tabA$k+1))/(n-tabA$k-1))
-#now order from smallest to biggest
-tabA=tabA[order(tabA$aiccs),]
-#calculate delta AIC
-tabA$dAIC = tabA$aiccs - min(tabA$aiccs)
-#you use the next two lines to get weights
-tabA$edel<-exp(-0.5*tabA$dAIC) 
-tabA$wt<-tabA$edel/sum(tabA$edel)
-tabA
-
