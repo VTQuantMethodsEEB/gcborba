c7555abe4fce6e5e0950582168473ed4cde8b064
gcborba <you@example.com>
2022-04-19 21:11:25 -0400

    update week 12

--- Week12_GabrielBorba.R
+++ Week12_GabrielBorba.R
@@ -2,25 +2,24 @@ rm(list=ls())
 
 ##load important packages##
 library(lme4)
-
+library(glmmTMB)
 #using the catch data - read
 catdat=read.csv("catch.csv") 
 
-#round my catch values 
+#round my catch values and scale predictors
 catdat$catch1 <- round(catdat$catch)
-catdat<-scale(cbind(catdat$effort,catdat$length))
+catdat$effort1<-scale(catdat$effort)
+catdat$length1<-scale(catdat$length)
+#including new columns 
 #H: fish catch depends on interaction between boat length and 
-#effort, taking account and controlling the distribution of my boat id.
+#effort among river sites, taking account and controlling the distribution of my boat id.
 
+#try glmm.tb
 ## run mixed model
-m.nb <- glmer.nb(catdat$catch1 ~ length*effort+(1|catdat$boat), data=cat.scaled)
-summary(m.nb)
-cat.scaled <- as.data.frame(scale(catdat[,c(5,10)]))
-catdat1<- scale(catdat)
-catdat$effort1 <- scale(effort)
+m.nb1 <- glmmTMB(catch1 ~ length1*effort1 + river + (1|boat), data=catdat, family=nbinom2)
 
 #look at mixed model
-summary(m.nb)
+summary(m.nb1)
 
 
 
